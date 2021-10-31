dad2e059944731f7047b84e8640b09ce878ab493
amitpj <65959008+amitpj@users.noreply.github.com>
2021-10-31 12:34:24 -0500

    Delete client.py

--- Chapter 9/client.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-server_url = 'http://localhost:5000/predict'
-path = 'cat-and-dog/test_set/test_set/cats/cat.4001.jpg'
-files = {'image': open(path, 'rb')}
-resp = requests.post(server_url, files=files)
-print(resp.json())
