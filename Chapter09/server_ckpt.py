b956c5dbaba09ba6e3d5f2950da0a25be6099804
amitpj <65959008+amitpj@users.noreply.github.com>
2021-12-22 21:01:06 -0600

    Add files via upload

--- Chapter09/server_ckpt.py
+++ Chapter09/server_ckpt.py
@@ -1,10 +1,9 @@
 # Important Note:
 #
-# For your convenience, we have copied the notebook named
-# "Cats and Dogs Classifier.ipynb" from Chapter 2 to the
-# GitHub folder for this chapter (only change in the notebook
-# is that ImageClassifier class is now defined in its own
+# For your convenience, we have copied the notebook named "Cats and Dogs Classifier.ipynb" from Chapter 2 to the
+# GitHub folder for this chapter (only change in the notebook is that ImageClassifier class is now defined in its own
 # file named "image_classifier.py", so we have removed that cell).
+#
 # Run that notebook before launching this server program.
 
 import torch.nn.functional as functional
